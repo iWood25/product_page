cb0030cd2a6cf83cc58ad2aaa003efb4139e1d9c
iwood25 <petr.ryabokon@gmail.com>
2016-06-23 00:06:37 +0300

    Connected mmenu, change bower.json, wrote js code to mmenu

--- ContentBuild/js/main.js
+++ ContentBuild/js/main.js
@@ -1,8 +1,28 @@
 ﻿$(function () {
 
+	// my-menu
+
+	$(".nav__list").after("<div id='my-menu'>");
+	$(".nav__list").clone().appendTo("#my-menu");
+	$("#my-menu").find("*").attr("style", "");
+	$("#my-menu").mmenu({
+		extensions: ['widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: "Menu"
+		}
+	});
+
+	var api = $("#my-menu").data("mmenu");
+	api.bind("closed", function () {
+		$(".toggle-mnu").removeClass("on");
+	});
+
 	// Toggle-mnu
-	$(".toggle-mnu").click(function () {
-		$(this).toggleClass("on");
+	$(".mobile-mnu").click(function () {
+		var mmAPI = $("#my-menu").data("mmenu");
+		mmAPI.open();
+		var thiss = $(this).find(".toggle-mnu");
+		thiss.toggleClass("on");
 		$(".main-mnu").slideToggle();
 		return false;
 	});
